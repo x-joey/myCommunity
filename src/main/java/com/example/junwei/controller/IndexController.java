370802add12aa966ebd030e1faf1c764c23dec69
x-joey <2535690564@qq.com>
2020-03-17 14:30:48 +0800

    前后端可以对应

--- src/main/java/com/example/junwei/controller/IndexController.java
+++ src/main/java/com/example/junwei/controller/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  *         model.addAttribute("name",name);
  *         return "index";
  *     }
+ *
  */
 @Controller
 public class IndexController {
