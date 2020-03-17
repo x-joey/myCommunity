6e1171861903904129e2766e20b0619756ec5af6
x-joey <2535690564@qq.com>
2020-03-17 10:59:04 +0800

    前后端可以对应

---
+++ src/main/java/com/example/junwei/JunweiApplication.java
@@ -0,0 +1,13 @@
+package com.example.junwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunweiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junwei/JunweiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunweiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
